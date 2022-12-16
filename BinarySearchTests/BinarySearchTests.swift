35550f18f948fe3e349a0236f00426e14f106f46
arifinfrds <arifinfrds.engineer@gmail.com>
2022-12-16 18:51:05 +0700

    implement `binarySearch` logic

--- BinarySearchTests/BinarySearchTests.swift
+++ BinarySearchTests/BinarySearchTests.swift
@@ -8,11 +8,33 @@
 import XCTest
 @testable import BinarySearch
 
+// [1, 3, 7] // sorting
+// while
+// target is in the middle? return middle number
+// if target < middleNumber, then maybe it is on the left side of middle number
+// if target > middleNumber, then maybe it is on the right side of middle number
+
 struct Program {
     func binarySearch(in array: [Int], target: Int) -> Int? {
         
-        for number in array where number == target {
-            return number
+        let sortedNumbers = array.sorted()
+        var leftPointerIndex = 0
+        var rightPointerIndex = sortedNumbers.count - 1
+        
+        while leftPointerIndex <= rightPointerIndex {
+            
+            let middleIndex = (leftPointerIndex + rightPointerIndex) / 2
+            let middleNumber = sortedNumbers[middleIndex]
+            
+            if target == middleNumber {
+                return middleNumber
+            }
+            if target < middleNumber {
+                rightPointerIndex = middleIndex - 1
+            }
+            if target > middleNumber {
+                leftPointerIndex = middleIndex + 1
+            }
         }
         
         return nil
@@ -51,9 +73,9 @@ final class BinarySearchTests: XCTestCase {
     func test_search_shouldReturnsFoundNumberWhenFoundNumberOnMoreThanOneArrayItem() {
         let sample = [1, 7, 3]
         let sut = Program()
-        
+
         let result = sut.binarySearch(in: sample, target: 7)
-        
+
         XCTAssertEqual(result, 7)
     }
 
